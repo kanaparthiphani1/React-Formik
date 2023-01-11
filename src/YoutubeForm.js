612f2c2a0c660e86bef6c56a58cc88ed006df21c
Kanaparthi Phani <38982795+kanaparthiphani1@users.noreply.github.com>
2023-01-11 18:47:46 +0530

    feat: Added new fields using renderProp and 'as'. revisited ErrorComponent

--- src/YoutubeForm.js
+++ src/YoutubeForm.js
@@ -5,7 +5,9 @@ import * as Yup from "yup";
 const initialValues = {
   name: "",
   email: "",
-  channel: ""
+  channel: "",
+  mobile: "",
+  comments: ""
 };
 
 const onSubmit = (values) => {
@@ -15,32 +17,51 @@ const onSubmit = (values) => {
 const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
   email: Yup.string().required("Required").email("Invalid Email Format"),
-  channel: Yup.string().required("Required")
+  channel: Yup.string().required("Required"),
+  mobile: Yup.string().required("Required"),
+  comments: Yup.string().required("Required")
 });
 
 export const YoutubeForm = () => {
   return (
     <Formik
-      className="form-outer"
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <Form>
+      <Form className="form-outer">
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component="p" />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email" component="p" />
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" />
-          <ErrorMessage name="channel" />
+          <ErrorMessage name="channel" component="p" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="mobile">Mobile</label>
+          <Field name="mobile">
+            {(props) => {
+              return <input type="text" {...props.field} />;
+            }}
+          </Field>
+          <ErrorMessage name="mobile" component="p" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+          <ErrorMessage name="comments">
+            {(errorMsg) => {
+              return <p className="error">{errorMsg}</p>;
+            }}
+          </ErrorMessage>
         </div>
 
         <button type="submit">Submit</button>
